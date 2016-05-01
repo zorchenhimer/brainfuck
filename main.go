fcd9f1101399792a8f347ead12f9c34667c8b4ea
Nick Scheibenpflug <zorchenhimer@gmail.com>
2016-04-30 20:38:26 -0400

    Get input file from command line

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
     "fmt"
     "log"
+    "os"
 
     "./bf"
 )
 
 func main() {
+    if len(os.Args) == 1 {
+        log.Fatal("Missing input file.")
+    } else if len(os.Args) > 2 {
+        log.Fatal("Too many input files.")
+    }
+
     engine := bf.NewEngine()
-    err := engine.Load("hello-world3.bf")
+    err := engine.Load(os.Args[1])
     if err != nil {
         log.Fatalf("Error loading source: %s", err)
     }
@@ -18,6 +25,4 @@ func main() {
         fmt.Println("")
         log.Fatalf("Run failed: %s\n", err.String())
     }
-    
-    fmt.Println("Done.")
 }
